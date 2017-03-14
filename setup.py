a5c4d050d4c058b2d7d4976127b7d916170d5279
Chris Hench <chris.l.hench@gmail.com>
2017-03-14 11:23:24 -0700

    prep for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='syllabipy',
+    packages=['syllabipy'],
+    version='0.1',
+    description='Universal syllabification algorithms',
+    author='Christopher Hench',
+    author_email='chris.l.hench@gmail.com',
+    url='https://github.com/henchc/syllabipy',
+    download_url='https://github.com/henchc/syllabipy/archive/0.1.tar.gz',
+    keywords=['syllabification', 'onset', 'sonority', 'ssp', 'legality'],
+    classifiers=[],
+)
